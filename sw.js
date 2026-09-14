0504101600ff33536b62c0994a6f19fb9330fc9c
ydv-satyamm <satyamyadavlibrary@gmail.com>
2026-09-14 11:45:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,39 @@
-// sw.js - Background Service Worker for Lock Screen Notifications
+const CACHE_NAME = 'study-portal-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://drive.google.com/thumbnail?id=1VC6Iw2foLA9FoGYgwNjU2AKrb2x_ZihX&sz=w512'
+];
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE).catch(() => {});
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-// Listen for background trigger from main thread
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'TRIGGER_SESSION_NOTIFICATION') {
-    const { title, body, icon } = event.data;
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon || 'https://cdn-icons-png.flaticon.com/512/3281/3281329.png',
-      badge: 'https://cdn-icons-png.flaticon.com/512/3281/3281329.png',
-      vibrate: [500, 250, 500, 250, 500],
-      tag: 'study-timer-complete',
-      renotify: true,
-      requireInteraction: true // Lock screen par notification tiki rahegi jab tak user interact na kare
-    });
-  }
-});
-
-// Notification click par app open karna
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
-      for (const client of clientList) {
-        if (client.url && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow('/');
-      }
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => response);
     })
   );
 });
