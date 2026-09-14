b6e782d42b30487a62855b496e42e4904401d210
ydv-satyamm <satyamyadavlibrary@gmail.com>
2026-09-14 20:21:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'study-portal-v1';
+const CACHE_NAME = 'study-portal-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  'https://drive.google.com/thumbnail?id=1VC6Iw2foLA9FoGYgwNjU2AKrb2x_ZihX&sz=w512'
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE).catch(() => {});
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
   self.skipWaiting();
@@ -33,7 +33,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => response);
+      return response || fetch(event.request);
     })
   );
 });
